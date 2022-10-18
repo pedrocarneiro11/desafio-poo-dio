db265b82bb9e5ea6787c8afca98a4df5803d6ad7
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-10-18 11:56:24 -0300

    Criacao de novas classes(Mentoria e Curso) e reorganizacao do codigo

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+package src;
+
+import src.br.com.dio.desafio.dominio.Curso;
+import src.br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso(); //instanciamento do objeto curso
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso(); //instanciamento do objeto curso
+        curso2.setTitulo("Curso Angular");
+        curso2.setDescricao("Descricao curso Angular");
+        curso2.setCargaHoraria(6);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria JavaScript");
+        mentoria.setDescricao("Descricao Mentoria JavaScript");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
